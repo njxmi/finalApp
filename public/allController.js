b9ed31aada66011eec489e4b96decc7156640020
njxmi <nicole.jones.mi@gmail.com>
2017-08-29 16:45:31 -0400

    upvote button works now

--- public/allController.js
+++ public/allController.js
@@ -9,9 +9,21 @@ app.controller("allController", function($scope, $http) {
 	$http.get('/api/items').then(function success(response) {
 		$scope.items = response.data;
 		console.log(response.data);
-	});
+	}); //this loads data, could use to refresh page below
 
 	
+	
+	$scope.upVote = function(id) {
+		console.log('clicked', id);
+		var serverUrl =	('/api/items/' + id + '/votes');
+		$http.put(serverUrl).then(function success(response) {
+			$http.get('/api/items').then(function success(response) {
+			$scope.items = response.data;
+			console.log(response.data);
+			}); //ideally would have a function you can call more than once, this was a quick way to make it work
+		});	
+		
+	};
 
 	
 	// Example from assessment:
